b2eb6c68d2ddbe963c9746cccb639f8d85a63fc5
Yuliana Komang <yulianakomanggede@gmail.com>
2016-08-11 12:14:20 +0800

    Add item

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 	$('#menu').click(function(){
 		$('#isi-menu').toggle('slow');
 	});
+	$('#content-php').masonry({
+          itemSelector: '.box-content',
+        });
+	$('#content-wp').masonry({
+          itemSelector: '.box-content',
+        });
 });
